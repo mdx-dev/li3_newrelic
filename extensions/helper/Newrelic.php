a3b333800f81a912c637d7b4f0549cdde64084c8
Nathan Phelps <me@nathanwphelps.com>
2012-07-06 07:45:20 -0500

    Add template helper for New Relic Real User Monitoring JavaScript

---
+++ extensions/helper/Newrelic.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace li3_newrelic\extensions\helper;
+
+use \lithium\template\Helper;
+
+class Newrelic extends Helper {
+
+	/**
+	 * Inserts New Relic "Real User Monitoring" header script when called. Should be placed in the header
+	 * @param bool $includeScriptTag
+	 * @return mixed
+	 */
+	public function headerJavaScript($includeScriptTag = true) {
+		if (Environment::is('production') && extension_loaded ('newrelic')) {
+		  return newrelic_get_browser_timing_header($includeScriptTag);
+		}
+	}
+
+	/**
+	 * Inserts New Relic "Real User Monitoring" footer script when called. Should be placed right before </body>
+	 * @param bool $includeScriptTag
+	 * @return mixed
+	 */
+	public function footerJavaScript($includeScriptTag = true) {
+		if (Environment::is('production') && extension_loaded ('newrelic')) {
+		    newrelic_get_browser_timing_footer($includeScriptTag);
+		}
+	}
+}
+?>
