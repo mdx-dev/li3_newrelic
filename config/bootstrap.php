187bd0012590362e009b5b0dc5c16fb7dc068132
Mehdi Lahmam B <mehdi@lahmam.com>
2013-01-14 18:00:11 +0100

    Fix a Strict standards warning

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -4,15 +4,16 @@ use lithium\core\Libraries;
 use lithium\action\Dispatcher;
 use lithium\core\Environment;
 
-Dispatcher::applyFilter('_callable', function($self, $params, $chain) {	
-	
+Dispatcher::applyFilter('_callable', function($self, $params, $chain) {
+
 	$controller = $chain->next($self, $params, $chain);
 
-	if (Environment::is('production') && extension_loaded ('newrelic')) {
-		$controllerName = preg_replace('/Controller$/', '', array_pop(explode('\\', get_class($controller))));
-		newrelic_name_transaction ($controllerName.'/'.$params['request']->params['action']);
+	if (Environment::is('production') && extension_loaded('newrelic')) {
+		$ctrl = explode('\\', get_class($controller));
+		$controllerName = preg_replace('/Controller$/', '', array_pop($ctrl));
+		newrelic_name_transaction(strtolower($controllerName) . '/' . $params['request']->params['action']);
 	}
-	
+
 	return $controller;
 });
 
