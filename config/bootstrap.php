699c1c501fe03ff331bec3454650ef76873c6b6c
Brandon Westcott <brandonwestcott@gmail.com>
2013-01-14 11:40:51 -0600

    Un-lowercase controller name for transaction

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -11,7 +11,7 @@ Dispatcher::applyFilter('_callable', function($self, $params, $chain) {
 	if (Environment::is('production') && extension_loaded('newrelic')) {
 		$ctrl = explode('\\', get_class($controller));
 		$controllerName = preg_replace('/Controller$/', '', array_pop($ctrl));
-		newrelic_name_transaction(strtolower($controllerName) . '/' . $params['request']->params['action']);
+		newrelic_name_transaction($controllerName . '/' . $params['request']->params['action']);
 	}
 
 	return $controller;
